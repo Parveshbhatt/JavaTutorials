83683865515a0c3ef142e99f091207e20354e9d3
Parveshbhatt <parveshbhatt118@gmail.com>
2021-07-26 23:19:00 +0530

    first commit, Session1

---
+++ Session1/src/Hello.java
@@ -0,0 +1,10 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!!, Welcome to java programming :)");
+		System.out.println("First code");
+
+	}
+
+}
