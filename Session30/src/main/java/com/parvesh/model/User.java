9c26832867264a5c3e69058d7d613b3847adfc33
Parveshbhatt <parveshbhatt118@gmail.com>
2021-09-14 07:28:30 +0530

    error page

---
+++ Session30/src/main/java/com/parvesh/model/User.java
@@ -0,0 +1,23 @@
+package com.parvesh.model;
+
+public class User {
+			
+	String name;
+	String email;
+	
+	public User() {
+		
+	}
+	
+	public User(String name, String email) {
+		this.name=name;
+		this.email=email;
+	}
+
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", email=" + email + "]";
+	}
+	
+	
+}
